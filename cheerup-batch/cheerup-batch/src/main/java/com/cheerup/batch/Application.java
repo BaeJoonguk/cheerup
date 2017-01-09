64ca19fd038ae702db81f0f6bb90f5448b70bf1d
miiiiinkee <lindakmk@naver.com>
2017-01-09 10:36:10 +0900

    메세지보내기 기능(라디오버튼)제어문걸어서 추가본 UI는 일단 여기까지

--- cheerup-batch/cheerup-batch/src/main/java/com/cheerup/batch/Application.java
+++ cheerup-batch/cheerup-batch/src/main/java/com/cheerup/batch/Application.java
@@ -23,8 +23,7 @@ public class Application {
 	}
 		
 	
-	
-	
+
 	public static void main(String[] args) throws Exception {
 	  ApplicationContext ctx = SpringApplication.run(Application.class, args);
 	}
--- cheerup_android/app/src/main/java/com/example/user/cheerup/Activity/WriteMessageActivity.java
+++ cheerup_android/app/src/main/java/com/example/user/cheerup/Activity/WriteMessageActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.user.cheerup.R;
@@ -12,6 +13,7 @@ import com.example.user.cheerup.R;
 public class WriteMessageActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button choose_button;
+    private EditText edit_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +22,25 @@ public class WriteMessageActivity extends AppCompatActivity implements View.OnCl
         initview();
     }
 
-    public void initview()
-    {
-        choose_button=(Button)findViewById(R.id.choose_button);
+    public void initview() {
+        choose_button = (Button) findViewById(R.id.choose_button);
         choose_button.setOnClickListener(this);
+        edit_message = (EditText) findViewById(R.id.edit_message);
     }
 
 
-    public void onClick(View v){
-        if(v==choose_button){
-            Intent intent =new Intent(getApplicationContext(),ChooseSendActivity.class);
-            startActivity(intent);
-            finish();
+    public void onClick(View v) {
+        if (v == choose_button) {
+            if (edit_message.getText().toString().equals("")) {
+                Toast.makeText(getApplicationContext(), "글을 입력해주세요", Toast.LENGTH_SHORT).show();
+            }
+
+            else {
+                Intent intent = new Intent(getApplicationContext(), ChooseSendActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
         }
     }
 }
