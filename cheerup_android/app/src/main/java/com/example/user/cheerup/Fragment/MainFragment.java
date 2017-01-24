e43db6a847b53b99ee96421655bc7abed103a757
BaeJoonguk <joonguk90@gmail.com>
2017-01-24 15:28:29 +0900

    MainFragment.java 수정 서버에서 카드 데이터 가져오는 부분 추가
    WriteQFragment.java 수정
    서버에 카드 데이터 입력하는 부분 추가

--- cheerup_android/app/src/main/java/com/example/user/cheerup/Fragment/MainFragment.java
+++ cheerup_android/app/src/main/java/com/example/user/cheerup/Fragment/MainFragment.java
@@ -1,18 +1,35 @@
 package com.example.user.cheerup.Fragment;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import com.example.user.cheerup.Activity.MainActivity;
 import com.example.user.cheerup.GetnSet.MainFragListItem;
 import com.example.user.cheerup.R;
 import com.example.user.cheerup.adapter.MainFragAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by user on 2017-01-02.
@@ -21,16 +38,26 @@ import java.util.ArrayList;
 
 public class MainFragment extends Fragment {
 
+    String myJSON;
+
+    private static final String TAG_RESULTS="result";
+    private static final String TAG_CARDNUMBER = "CardNumber";
+    private static final String TAG_CONTENTS ="Contents";
+    private static final String TAG_WRITER ="Writer";
+
+    JSONArray cards = null;
+
+    RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View v =inflater.inflate(R.layout.main_frag,null);
 
-        //Recyclerview 띄우기
-        RecyclerView recyclerView=(RecyclerView) v.findViewById(R.id.main_recycler);
+        recyclerView=(RecyclerView) v.findViewById(R.id.main_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-        recyclerView.setAdapter(new MainFragAdapter(this.getActivity(),getMainView()));
+
+        getData("http://IP Address/getdata.php");
 
         return v;
     }
@@ -64,5 +91,77 @@ public class MainFragment extends Fragment {
         return "홈";
     }
 
-}
 
+    protected void showList(){
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            cards = jsonObj.getJSONArray(TAG_RESULTS);
+
+            //collection of cardData
+            ArrayList<MainFragListItem> ListData =new ArrayList<>();
+            MainFragListItem cardData;
+
+            for(int i=0;i<cards.length();i++){
+
+                JSONObject c = cards.getJSONObject(i);
+                String cardnumber = c.getString(TAG_CARDNUMBER);
+                String contents = c.getString(TAG_CONTENTS);
+                String writer = c.getString(TAG_WRITER);
+
+                //single cardData
+                cardData=new MainFragListItem(contents);//(id,내용,글쓴이)
+                //add to collection
+                ListData.add(cardData);
+            }
+
+            recyclerView.setAdapter(new MainFragAdapter(this.getActivity(),ListData));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void getData(String url){
+
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+
+                    while((json = bufferedReader.readLine())!= null){
+                        sb.append(json+"\n");
+                    }
+
+                    return sb.toString().trim();
+
+                }catch(Exception e){
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                myJSON=result;
+                showList();
+            }
+        }
+
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+}
--- cheerup_android/app/src/main/java/com/example/user/cheerup/Fragment/WriteQFragment.java
+++ cheerup_android/app/src/main/java/com/example/user/cheerup/Fragment/WriteQFragment.java
@@ -1,19 +1,29 @@
 package com.example.user.cheerup.Fragment;
 
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.user.cheerup.Activity.MainActivity;
 import com.example.user.cheerup.Activity.WriteQActivity;
 import com.example.user.cheerup.R;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 
 /**
@@ -21,6 +31,8 @@ import com.example.user.cheerup.R;
  */
 
 public class WriteQFragment extends Fragment {
+
+    private EditText editTextContents;
 
     public WriteQFragment() {
         // Required empty public constructor
@@ -31,16 +43,84 @@ public class WriteQFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    private void insertToDatabase(String contents, String writer){
+
+        class InsertData extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(getContext(), "Please Wait", null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                try{
+                    String Contents = (String)params[0];
+                    String Writer = (String)params[1];
+
+                    String link="http://IP Address/insert.php";
+                    String data  = URLEncoder.encode("Contents", "UTF-8") + "=" + URLEncoder.encode(Contents, "UTF-8");
+                    data += "&" + URLEncoder.encode("Writer", "UTF-8") + "=" + URLEncoder.encode(Writer, "UTF-8");
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write( data );
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while((line = reader.readLine()) != null)
+                    {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                }
+                catch(Exception e){
+                    return new String("Exception: " + e.getMessage());
+                }
+
+            }
+        }
+
+        InsertData task = new InsertData();
+        task.execute(contents,writer);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View root=inflater.inflate(R.layout.writeq_frag, container, false);
         Button write_button=(Button)root.findViewById(R.id.write_button) ;
+
+        editTextContents = (EditText) root.findViewById(R.id.Q_editText);
+
         write_button.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v){
+
+                String contents = editTextContents.getText().toString();
+                insertToDatabase(contents, "writer");
+
                 Intent intent=new Intent(getActivity(), MainActivity.class);
                 //Toast.makeText(,"질문등록 완료", Toast.LENGTH_SHORT).show();
                 startActivity(intent);
